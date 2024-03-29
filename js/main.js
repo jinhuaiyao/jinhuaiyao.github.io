f068af6e486011a82fd841a79e9a88dcc78e44e3
jinhuaiyao <jinhuaiyao@gmail.com>
2024-03-29 08:29:51 +0800

    update

--- js/main.js
+++ js/main.js
@@ -52,8 +52,8 @@ var memo = {
     limit: '10',
     creatorId: '101',
     domId: '#memos',
-    username: 'Admin',
-    name: 'Administrator'
+    username: '',
+    name: ''
 }
 if (typeof (memos) !== "undefined") {
     for (var key in memos) {
@@ -73,7 +73,7 @@ var page = 1,
 var tag='';
 var btnRemove = 0
 var memoDom = document.querySelector(memo.domId);
-var load = '<button class="load-btn button-load">努力加载中……</button>'
+var load = '<button class="load-btn button-load">loading……</button>'
 if (memoDom) {
     memoDom.insertAdjacentHTML('afterend', load);
     getFirstList() // 首次加载数据
@@ -81,7 +81,7 @@ if (memoDom) {
     btnRemove = 0;
     var btn = document.querySelector("button.button-load");
     btn.addEventListener("click", function () {
-        btn.textContent = '努力加载中……';
+        btn.textContent = 'loading……';
         updateHTMl(nextDom)
         if (nextLength < limit) { // 返回数据条数小于限制条数，隐藏
             document.querySelector("button.button-load").remove()
@@ -140,7 +140,7 @@ document.addEventListener('click', function (event) {
             // 添加 button 事件监听器
             var btn = document.querySelector("button.button-load");
             btn.addEventListener("click", function () {
-                btn.textContent = '努力加载中……';
+                btn.textContent = 'loading……';
                 updateHTMl(nextDom)
                 if (nextLength < limit) { // 返回数据条数小于限制条数，隐藏
                     document.querySelector("button.button-load").remove()
@@ -220,7 +220,7 @@ function updateHTMl(data) {
     // Memos Content
     for (var i = 0; i < data.length; i++) {
         var memoContREG = data[i].content
-            .replace(TAG_REG, "<span class='tag-span'><a rel='noopener noreferrer' href='#$1'>#$1</a></span>")
+            .replace(TAG_REG, "<span class='tag-span'><a rel='noopener noreferrer' href='#$1'>#$1 </a></span>")
 
         // For CJK language users
         // 用 PanguJS 自动处理中英文混合排版
@@ -268,7 +268,7 @@ function updateHTMl(data) {
                 memoContREG += '<div class="resource-wrapper "><p class="datasource">' + resUrl + '</p></div>'
             }
         }
-        memoResult += '<li class="timeline"><div class="memos__content"><div class="memos__text"><div class="memos__userinfo"><div>' + memo.name + '</div><div><svg viewBox="0 0 24 24" aria-label="认证账号" class="memos__verify"><g><path d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"></path></g></svg></div><div class="memos__id">@' + memo.username + '</div></div><p>' + memoContREG + '</p></div><div class="memos__meta"><small class="memos__date">' + moment(data[i].createdTs * 1000).twitter() + ' • 来自「<a href="' + memo.host + 'm/' + data[i].id + '" target="_blank">Memos</a>」</small></div></div></li>'
+        memoResult += '<li class="timeline"><div class="memos__content"><div class="memos__text"><div class="memos__userinfo"><div>' + memo.name + '</div><div><svg viewBox="0 0 24 24" aria-label="认证账号" class="memos__verify"><g><path d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"></path></g></svg></div><div class="memos__id">' + memo.username + '</div></div><p>' + memoContREG + '</p></div><div class="memos__meta"><small class="memos__date">' + moment(data[i].createdTs * 1000).twitter() + '</small></div></div></li>'
     }
     var memoBefore = '<ul class="">'
     var memoAfter = '</ul>'
@@ -280,124 +280,3 @@ function updateHTMl(data) {
 }
 // Memos End
 
-// 解析豆瓣 Start
-// 文章内显示豆瓣条目 https://immmmm.com/post-show-douban-item/
-// 解析豆瓣必须要API，请找朋友要权限，或自己按 https://github.com/eallion/douban-api-rs 这个架设 API，非常简单，资源消耗很少
-// 已内置样式，修改 API 即可使用
-function fetchDB() {
-    var dbAPI = "https://api.example.com/";  // 修改为自己的 API
-    var dbA = document.querySelectorAll(".timeline a[href*='douban.com/subject/']:not([rel='noreferrer'])") || '';
-    if (dbA) {
-        for (var i = 0; i < dbA.length; i++) {
-            _this = dbA[i]
-            var dbHref = _this.href
-            var db_reg = /^https\:\/\/(movie|book)\.douban\.com\/subject\/([0-9]+)\/?/;
-            var db_type = dbHref.replace(db_reg, "$1");
-            var db_id = dbHref.replace(db_reg, "$2").toString();
-            if (db_type == 'movie') {
-                var this_item = 'movie' + db_id;
-                var url = dbAPI + "movies/" + db_id;
-                if (localStorage.getItem(this_item) == null || localStorage.getItem(this_item) == 'undefined') {
-                    fetch(url).then(res => res.json()).then(data => {
-                        let fetch_item = 'movies' + data.sid;
-                        let fetch_href = "https://movie.douban.com/subject/" + data.sid + "/"
-                        localStorage.setItem(fetch_item, JSON.stringify(data));
-                        movieShow(fetch_href, fetch_item)
-                    });
-                } else {
-                    movieShow(dbHref, this_item)
-                }
-            } else if (db_type == 'book') {
-                var this_item = 'book' + db_id;
-                var url = dbAPI + "v2/book/id/" + db_id;
-                if (localStorage.getItem(this_item) == null || localStorage.getItem(this_item) == 'undefined') {
-                    fetch(url).then(res => res.json()).then(data => {
-                        let fetch_item = 'book' + data.id;
-                        let fetch_href = "https://book.douban.com/subject/" + data.id + "/"
-                        localStorage.setItem(fetch_item, JSON.stringify(data));
-                        bookShow(fetch_href, fetch_item)
-                    });
-                } else {
-                    bookShow(dbHref, this_item)
-                }
-            }
-        }// for end
-    }
-}
-
-function movieShow(fetch_href, fetch_item) {
-    var storage = localStorage.getItem(fetch_item);
-    var data = JSON.parse(storage);
-    var db_star = Math.ceil(data.rating);
-    var db_html = "<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' rel='noreferrer' href='" + fetch_href + "'>《" + data.name + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>导演：" + data.director + " / 类型：" + data.genre + " / " + data.year + "</time><section class='post-preview--excerpt'>" + data.intro.replace(/\s*/g, "") + "</section></div></div><img referrer-policy='no-referrer' loading='lazy' class='post-preview--image' src=" + data.img + "></div>"
-    var db_div = document.createElement("div");
-    var qs_href = ".timeline a[href='" + fetch_href + "']"
-    var qs_dom = document.querySelector(qs_href)
-    qs_dom.parentNode.replaceChild(db_div, qs_dom);
-    db_div.innerHTML = db_html
-}
-
-function bookShow(fetch_href, fetch_item) {
-    var storage = localStorage.getItem(fetch_item);
-    var data = JSON.parse(storage);
-    var db_star = Math.ceil(data.rating.average);
-    var db_html = "<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' rel='noreferrer' href='" + fetch_href + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating.average + "</div></div><time class='post-preview--date'>作者：" + data.author + " </time><section class='post-preview--excerpt'>" + data.summary.replace(/\s*/g, "") + "</section></div></div><img referrer-policy='no-referrer' loading='lazy' class='post-preview--image' src=" + data.images.medium + "></div>"
-    var db_div = document.createElement("div");
-    var qs_href = ".timeline a[href='" + fetch_href + "']"
-    var qs_dom = document.querySelector(qs_href)
-    qs_dom.parentNode.replaceChild(db_div, qs_dom);
-    db_div.innerHTML = db_html
-}
-// 解析豆瓣 End
-
-// Images lightbox
-window.ViewImage && ViewImage.init('.container img');
-
-// Memos Total Start
-// Get Memos total count
-function getTotal() {
-    var totalUrl = memos + "/api/v1/memo/stats?creatorId=" + memo.creatorId
-    fetch(totalUrl).then(res => res.json()).then(resdata => {
-        if (resdata) {
-            var allnums = resdata.length
-            var memosCount = document.getElementById('total');
-            memosCount.innerHTML = allnums;
-        }
-    }).catch(err => {
-        // Do something for an error here
-    });
-};
-window.onload = getTotal();
-// Memos Total End
-
-// Toggle Darkmode
-const localTheme = window.localStorage && window.localStorage.getItem("theme");
-const themeToggle = document.querySelector(".theme-toggle");
-
-if (localTheme) {
-    document.body.classList.remove("light-theme", "dark-theme");
-    document.body.classList.add(localTheme);
-}
-
-themeToggle.addEventListener("click", () => {
-    const themeUndefined = !new RegExp("(dark|light)-theme").test(document.body.className);
-    const isOSDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-    if (themeUndefined) {
-        if (isOSDark) {
-            document.body.classList.add("light-theme");
-        } else {
-            document.body.classList.add("dark-theme");
-        }
-    } else {
-        document.body.classList.toggle("light-theme");
-        document.body.classList.toggle("dark-theme");
-    }
-
-    window.localStorage &&
-        window.localStorage.setItem(
-            "theme",
-            document.body.classList.contains("dark-theme") ? "dark-theme" : "light-theme",
-        );
-});
-// Darkmode End
