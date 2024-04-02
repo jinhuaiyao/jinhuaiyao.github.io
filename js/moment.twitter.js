78bab6bb3ee4c9b2c2f31a9fd9ad8e69bde4de85
jinhuaiyao <jinhuaiyao@gmail.com>
2024-04-02 17:29:57 +0800

    commit

--- js/moment.twitter.js
+++ js/moment.twitter.js
@@ -1,22 +1,3 @@
-// 处理发布时间 moment.js
-// moment.js locale
-moment.updateLocale('zh-cn', {
-    meridiem: function (hour, minute, isLowercase) {
-        if (hour < 6) {
-            return "Before dawn";
-        } else if (hour < 9) {
-            return "Early morning";
-        } else if (hour < 11 && minute < 30) {
-            return "Morning";
-        } else if (hour < 13 && minute < 30) {
-            return "Noon";
-        } else if (hour < 18) {
-            return "Afternoon";
-        } else {
-            return "Evening";
-        }
-    }
-});
 // moment.js twitter plugin
 (function () {
     var day, formats, hour, initialize, minute, second, week;
@@ -48,46 +29,13 @@ moment.updateLocale('zh-cn', {
     initialize = function (moment) {
         var twitterFormat;
         twitterFormat = function (format) {
-            var diff, num, unit, unitStr;
-            diff = Math.abs(this.diff(moment()));
-            unit = null;
-            num = null;
-            if (diff <= second) {
-                unit = 'seconds';
-                num = 1;
-            } else if (diff < minute) {
-                unit = 'seconds';
-            } else if (diff < hour) {
-                unit = 'minutes';
-            } else if (diff < day) {
-                unit = 'hours';
-            } else if (format === 'long') {
-                if (diff < week) {
-                    unit = 'days';
-                } else if (this.year() == year) {
-                    return this.format('YYYY.MM.DD HH:mm');
-                } else {
-                    return this.format('YYYY.MM.DD HH:mm');
-                }
-            } else {
-                return this.format('YYYY.MM.DD HH:mm');
-            }
-            if (!(num && unit)) {
-                num = moment.duration(diff)[unit]();
-            }
-            unitStr = unit = formats[unit][format];
-            if (format === 'long' && num > 1) {
-                unitStr += 's ago';
-            } else {
-                unitStr += ' ago';
-            }
-            return num + unitStr;
+            return this.format('YYYY.MM.DD HH:mm');
         };
         moment.fn.twitterLong = function () {
             return twitterFormat.call(this, 'long');
         };
         moment.fn.twitter = moment.fn.twitterShort = function () {
-            return twitterFormat.call(this, 'short');
+            return twitterFormat.call(this, 'long');
         };
         return moment;
     };
