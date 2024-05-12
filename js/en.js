50b8d10f05518c8746d4f0441361acc748bf23e0
jinhuaiyao <jinhuaiyao@gmail.com>
2024-05-12 18:11:32 +0800

    update

--- js/en.js
+++ js/en.js
@@ -1,39 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const contentDiv = document.getElementById('content');
+    const randomDiv = document.getElementById('randomLine');
+    const allLinesDiv = document.getElementById('allLines');
 
     // 替换这个URL为你的文本文件的实际URL
     const url = 'https://utils.jinhuaiyao.com/english.txt';
 
-
     fetch(url)
     .then(response => response.text())
     .then(text => {
-        // 处理文件内容，假设每行是一个条目
         const lines = text.split('\n');
+        const randomIndex = Math.floor(Math.random() * lines.length); // 随机选择一行
+
         lines.forEach((line, index) => {
             const parts = line.split('|');
             if (parts.length === 2) {
                 const chinesePart = parts[0].trim();
                 const englishPart = parts[1].trim();
 
-                const lineNumber = index + 1; // 序号从1开始
-                const lineDiv = document.createElement('div');
-                const lineHeader = document.createTextNode(lineNumber + '. ' + chinesePart + ' | - ');
+                const lineNumber = index + 1;
+                const lineHeader = document.createTextNode(lineNumber + '. ' + chinesePart + ' | ');
                 const toggleSpan = document.createElement('span');
-                toggleSpan.textContent = '- ';
-                toggleSpan.style.cursor = 'pointer';  // 使其看起来可以点击
+                toggleSpan.textContent = 'XXXX';
+                toggleSpan.style.cursor = 'pointer';
                 toggleSpan.onclick = function() {
-                    this.textContent = this.textContent === '- ' ? englishPart : '- ';
+                    this.textContent = this.textContent === 'XXXX' ? englishPart : 'XXXX';
                 };
 
+                const lineDiv = document.createElement('div');
                 lineDiv.appendChild(lineHeader);
                 lineDiv.appendChild(toggleSpan);
-                contentDiv.appendChild(lineDiv);
+
+                // 将随机选定的行显示在randomDiv中，其他行显示在allLinesDiv中
+                if (index === randomIndex) {
+                    randomDiv.appendChild(document.createTextNode(lineNumber + '. ' + chinesePart + ' | ' + englishPart));
+                } else {
+                    allLinesDiv.appendChild(lineDiv);
+                }
             }
         });
     })
     .catch(error => {
         console.error('Error loading the text file:', error);
-        contentDiv.textContent = 'Failed to load text data.';
+        randomDiv.textContent = 'Failed to load text data.';
     });
 });
